04b2f98394f8822c6610afb7639744b381606aa9
4NK <89443214+4NK001@users.noreply.github.com>
2024-08-07 22:31:07 +0530

    Add files via upload

--- bookshop.sql
+++ bookshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 16, 2024 at 07:41 PM
+-- Generation Time: Aug 07, 2024 at 06:54 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -40,8 +40,7 @@ CREATE TABLE `tbl_author` (
 --
 
 INSERT INTO `tbl_author` (`authorid`, `name`, `password`, `email`, `image`) VALUES
-(4, 'author', 'wsx', 'author@gmail.com', 'th.jfif'),
-(5, 'yellow', 'wsx', 'yellow@gmail.com', 'stack-old-books-light-beige-background-copyspace-angle-view-96885880.webp');
+(4, 'author', 'wsx', 'author@gmail.com', 'th.jfif');
 
 -- --------------------------------------------------------
 
@@ -55,15 +54,16 @@ CREATE TABLE `tbl_book` (
   `title` varchar(100) NOT NULL,
   `description` varchar(500) NOT NULL,
   `categoryid` int(10) NOT NULL,
-  `image` varchar(100) NOT NULL
+  `image` varchar(100) NOT NULL,
+  `bookfile` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tbl_book`
 --
 
-INSERT INTO `tbl_book` (`bookid`, `authorid`, `title`, `description`, `categoryid`, `image`) VALUES
-(9, 0, 'rose', 'rose', 1, 'rose.jpg');
+INSERT INTO `tbl_book` (`bookid`, `authorid`, `title`, `description`, `categoryid`, `image`, `bookfile`) VALUES
+(13, 4, 'goatlife', 'sadfkjlhgdfsghjgfdghj', 3, 'rose.jpg', 'Mypdf.pdf');
 
 -- --------------------------------------------------------
 
@@ -81,8 +81,7 @@ CREATE TABLE `tbl_category` (
 --
 
 INSERT INTO `tbl_category` (`categoryid`, `categoryname`) VALUES
-(1, 'Action'),
-(2, 'Comedy'),
+(5, 'novel'),
 (3, 'Sci-fi');
 
 -- --------------------------------------------------------
@@ -127,8 +126,19 @@ CREATE TABLE `tbl_reader` (
 
 INSERT INTO `tbl_reader` (`userid`, `name`, `email`) VALUES
 (5, 'abcd', 'abcd@gmail.com'),
-(6, 'yellow234', 'admin@gmail.com'),
-(7, '23423', 'admin12@gmail.com');
+(6, 'yellow234', 'admin@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_reader_book`
+--
+
+CREATE TABLE `tbl_reader_book` (
+  `reader_book_id` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `bookid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexes for dumped tables
@@ -171,6 +181,14 @@ ALTER TABLE `tbl_reader`
   ADD UNIQUE KEY `email` (`email`);
 
 --
+-- Indexes for table `tbl_reader_book`
+--
+ALTER TABLE `tbl_reader_book`
+  ADD PRIMARY KEY (`reader_book_id`),
+  ADD KEY `bookid` (`bookid`),
+  ADD KEY `userid` (`userid`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -184,13 +202,13 @@ ALTER TABLE `tbl_author`
 -- AUTO_INCREMENT for table `tbl_book`
 --
 ALTER TABLE `tbl_book`
-  MODIFY `bookid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `bookid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `tbl_category`
 --
 ALTER TABLE `tbl_category`
-  MODIFY `categoryid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `categoryid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `tbl_login`
@@ -203,6 +221,30 @@ ALTER TABLE `tbl_login`
 --
 ALTER TABLE `tbl_reader`
   MODIFY `userid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `tbl_reader_book`
+--
+ALTER TABLE `tbl_reader_book`
+  MODIFY `reader_book_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tbl_book`
+--
+ALTER TABLE `tbl_book`
+  ADD CONSTRAINT `tbl_book_ibfk_1` FOREIGN KEY (`authorid`) REFERENCES `tbl_author` (`authorid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tbl_book_ibfk_2` FOREIGN KEY (`categoryid`) REFERENCES `tbl_category` (`categoryid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tbl_reader_book`
+--
+ALTER TABLE `tbl_reader_book`
+  ADD CONSTRAINT `tbl_reader_book_ibfk_1` FOREIGN KEY (`bookid`) REFERENCES `tbl_book` (`bookid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tbl_reader_book_ibfk_2` FOREIGN KEY (`userid`) REFERENCES `tbl_reader` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
