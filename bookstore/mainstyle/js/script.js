db72f98ee5cf731cdee8eb62605e76b4c2dc85a6
4NK <89443214+4NK001@users.noreply.github.com>
2024-08-07 22:56:59 +0530

    Add files via upload

--- bookstore/mainstyle/js/script.js
+++ bookstore/mainstyle/js/script.js
@@ -7,7 +7,8 @@ document.querySelector('#search-btn').onclick = () =>{
 let loginForm = document.querySelector('.login-form-container');
 
 document.querySelector('#login-btn').onclick = () =>{
-  loginForm.classList.toggle('active');
+  window.location.href="./login.php";
+  // loginForm.classList.toggle('active');
 }
 
 document.querySelector('#close-login-btn').onclick = () =>{
